6b6c55be25c18b6d91d25c3bd15e685c79e408b0
Silvie Chlupova <sisi.chlupova@gmail.com>
2019-12-08 20:12:16 +0100

    Combobox

--- kidary/src/calendar/Controllers/createNewEventController.java
+++ kidary/src/calendar/Controllers/createNewEventController.java
@@ -10,13 +10,17 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ColorPicker;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
 import java.io.IOException;
 import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.ZoneId;
 import java.util.Date;
 import java.util.ResourceBundle;
@@ -24,6 +28,9 @@ import java.util.ResourceBundle;
 import static calendar.Controllers.Kidary.getData;
 
 public class createNewEventController implements Initializable {
+    Date from = null;
+    Date to = null;
+
     @FXML
     public JFXButton buttonBack;
     @FXML
@@ -35,11 +42,7 @@ public class createNewEventController implements Initializable {
     @FXML
     public DatePicker date;
     @FXML
-    public DatePicker from;
-    @FXML
     public JFXTimePicker fromTime;
-    @FXML
-    public DatePicker to;
     @FXML
     public JFXTimePicker toTime;
     @FXML
@@ -50,11 +53,14 @@ public class createNewEventController implements Initializable {
     public JFXButton buttonCreate;
     @FXML
     public ColorPicker colorPicker;
+    @FXML
+    public ComboBox<Label> To;
+    @FXML
+    public ComboBox<Label> From;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
-
+        addTimes();
 
         buttonBack.setOnAction(new EventHandler<ActionEvent>() {
             @Override
@@ -72,7 +78,17 @@ public class createNewEventController implements Initializable {
         buttonCreate.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
-
+                if (From.getValue() != null && To.getValue() != null) {
+                    String selectedFrom = From.getValue().getText();
+                    String selectedTo = To.getValue().getText();
+                    SimpleDateFormat formatter = new SimpleDateFormat("HH:mm");
+                    try {
+                        from = formatter.parse(selectedFrom);
+                        to = formatter.parse(selectedTo);
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                }
                 try {
                     Event e = new Event();
                     e.Id = getData().a.size()+1;
@@ -80,8 +96,8 @@ public class createNewEventController implements Initializable {
                     e.Type = type.getText();
                     e.Contacts = contact.getText();
                     e.Date = date.getValue();
-                    e.From = Date.from(from.getValue().atStartOfDay(ZoneId.systemDefault()).toInstant());
-                    e.To = new Date(Date.from(to.getValue().atStartOfDay(ZoneId.systemDefault()).toInstant()).getTime()+7200000);
+                    e.From = from;
+                    e.To = to;
                     e.Place = place.getText();
                     e.Note = note.getText();
                     e.Color = toRGBCode(colorPicker.getValue());
@@ -100,6 +116,33 @@ public class createNewEventController implements Initializable {
                 }
             }
         });
+    }
+
+    private void addTimes() {
+        for (int i = 0; i <= 9; i++) {
+            String time = "0" + Integer.toString(i) + ":" + "00";
+            Label label = new Label(time);
+            label.setStyle("-fx-text-fill: black");
+            From.getItems().add(label);
+        }
+        for (int i = 10; i < 24; i++) {
+            String time = Integer.toString(i) + ":" + "00";
+            Label label = new Label(time);
+            label.setStyle("-fx-text-fill: black");
+            From.getItems().add(label);
+        }
+        for (int i = 0; i <= 9; i++) {
+            String time = "0" + Integer.toString(i) + ":" + "00";
+            Label label = new Label(time);
+            label.setStyle("-fx-text-fill: black");
+            To.getItems().add(label);
+        }
+        for (int i = 10; i < 24; i++) {
+            String time = Integer.toString(i) + ":" + "00";
+            Label label = new Label(time);
+            label.setStyle("-fx-text-fill: black");
+            To.getItems().add(label);
+        }
     }
 
     public static String toRGBCode( Color color )
